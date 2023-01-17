0c170c2f08c4708187bffc09cf66b04ef67cb7f3
Thomas Behrens <info@tb80.de>
2023-01-17 20:57:05 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CriticalMass',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
